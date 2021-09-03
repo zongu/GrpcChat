5e8751e0111741eea14a4f6cafea647d1697127c
carter <zongu3317@gmail.com>
2021-09-03 16:34:03 +0800

    更新 server加入聊天邏輯

---
+++ GrpcChat.Server/ActionHandler/ChatMessageActionHandler.cs
@@ -0,0 +1,39 @@
+﻿
+namespace GrpcChat.Server.ActionHandler
+{
+    using System;
+    using GrpcChat.Server.Model;
+    using GrpcChat.Server.Model.Service;
+    using GrpcChat.Service;
+    using Microsoft.Extensions.Logging;
+    using NetCoreGrpc.Action;
+    using Newtonsoft.Json;
+
+    public class ChatMessageActionHandler : IActionHandler
+    {
+        private ILogger<ChatMessageActionHandler> logger;
+
+        private IClientShip clientShip;
+
+        public ChatMessageActionHandler(ILogger<ChatMessageActionHandler> logger, IClientShip clientShip)
+        {
+            this.logger = logger;
+            this.clientShip = clientShip;
+        }
+
+        public bool Execute(ActionModel actionModel)
+        {
+            try
+            {
+                var action = JsonConvert.DeserializeObject<ChatMessageAction>(actionModel.Content);
+                this.clientShip.Boradcast(action);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex, $"{this.GetType().Name} Execute Exception Content:{actionModel.Content}");
+                return false;
+            }
+        }
+    }
+}
